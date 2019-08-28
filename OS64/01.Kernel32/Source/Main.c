ecb81fa9beb25fd221198a7ea771b4fcc69af9f4
dwordptr <bsms0418@icloud.com>
2019-08-28 11:02:50 +0000

    Add Initailize Kerenl Function Kernel32/Main.c

--- OS64/01.Kernel32/Source/Main.c
+++ OS64/01.Kernel32/Source/Main.c
@@ -1,9 +1,14 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
-
+BOOL kInitializeKernel64Area(void);
 void Main(void){
+	DWORD i;
+
+
 	kPrintString(0, 3, "Kernel32 Started");
+	kInitializeKernel64Area();
+	kPrintString(0,4, "IA-32e Kernel Area Initialization Complete");
 
 	while(1);
 }
@@ -18,3 +23,24 @@ void kPrintString(int iX, int iY, const char* pcString){
 	}
 
 }
+
+//Initialize Kernel64 Area to 0
+BOOL kInitializeKernel64Area(void){
+	DWORD* pdwCurrentAddress;
+
+	// 1MB~6MB AREA
+	pdwCurrentAddress = (DWORD*) 0x100000;
+	while((DWORD) pdwCurrentAddress < 0x600000){
+		*pdwCurrentAddress = 0x00;
+		// if Current Address is not 0x00, problem to read address
+		if(*pdwCurrentAddress != 0){
+			return FALSE;
+		}
+		//Next address
+		pdwCurrentAddress++;
+	}
+
+	return TRUE;
+}
+
+
