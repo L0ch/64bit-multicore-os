e348452ce64d36e085a641954e025fc968eacbc7
dwordptr <bsms0418@icloud.com>
2019-08-30 11:54:20 +0000

    Add paging Kernel32/Page.h, Page.c

--- OS64/01.Kernel32/Source/Main.c
+++ OS64/01.Kernel32/Source/Main.c
@@ -3,14 +3,16 @@
 void kPrintString(int iX, int iY, const char* pcString);
 BOOL kInitializeKernel64Area(void);
 BOOL kIsMemoryEnough(void);
-void Main(void){
-	DWORD i;
 
+
+void Main(void){
+
+	DWORD i;
 
 	kPrintString(0, 3, "Kernel32 Started");
 
 	kPrintString(0, 4, "Minimum Memory Size Check..............[    ]");
-	if(kIsMemoryEnough() == FALSE){
+	if(kIsMemoryEnough() == FALSE){		//If memory less than 64Mbyte
 		kPrintString(40, 4, "Fail");
 		kPrintString(0, 5, "Not Enough Memory. System Requires Over 64Mbyte Memory" );
 		while(1);
@@ -19,7 +21,7 @@ void Main(void){
 		kPrintString(40, 4, "Done");
 	}
 	kPrintString(0,5, "IA-32e Kernel Area Initialization......[    ]");
-	if(kInitializeKernel64Area()==FALSE){
+	if(kInitializeKernel64Area()==FALSE){	//If can't access Kernel Area
 		kPrintString(40, 5, "Fail");
 		kPrintString(0, 6, "A-32e Kernel Area Initialization Fail");
 		while(1);
@@ -63,16 +65,18 @@ BOOL kInitializeKernel64Area(void){
 
 BOOL kIsMemoryEnough(void){
 	DWORD* pdwCurrentAddress;
-
+	//Start 1Mbyte
 	pdwCurrentAddress = (DWORD*) 0x100000;
 
+	//Until 64Mbyte
 	while((DWORD) pdwCurrentAddress < 0x4000000){
 		*pdwCurrentAddress = 0xdeadbeef;
 
+		// If Current Address is not 0xdeadbeef(can't read address), memory less than 64Mbyte
 		if (*pdwCurrentAddress != 0xdeadbeef){
 			return FALSE;
 		}
-
+		// next 1MB
 		pdwCurrentAddress += 0x100000;
 	}
 	return TRUE;
---
+++ OS64/01.Kernel32/Source/Page.c
@@ -0,0 +1,49 @@
+#include "Page.h"
+
+// Create page table for IA-32e
+void kInitializePageTables(void){
+	PML4TENTRY* pstPML4TEntry;
+	PDPTENTRY* pstPDPTEntry;
+	PDENTRY* pstPDEntry;
+	DWORD dwMappingAddress;
+	int i;
+
+	// PML4 Table
+	// Initialize to 0 except first entry
+	pstPML4TEntry = (PML4TENTRY*) 0x100000;
+	kSetPageEntryData( &(pstPML4TEntry[0]), 0x00, 0x101000, PAGE_FLAGS_DEFAULT, 0 );
+	for(i = 1; i < PAGE_MAXENTRYCOUNT; i++){
+			kSetPageEntryData( &(pstPML4TEntry[i]), 0, 0, 0, 0);
+	}
+
+	// Page Directory Pointer Table
+	// 64 Entry
+	pstPDPTEntry = (PDPTENTRY*) 0x101000; // 0x100000 + 4KB(0x1000)
+	for( i = 0; i < 64; i++){
+		kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0x102000 + (i * PAGE_TABLESIZE), PAGE_FLAGS_DEFAULT, 0);
+	}
+
+	for( i = 64; i < PAGE_MAXENTRYCOUNT; i++ ){
+		kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0, 0, 0);
+	}
+
+	// Page Directory Table
+	// 64(Each Page Directory can manage 1GB memory, Total 64GB)
+	pstPDEntry = (PDENTRY*) 0x102000; // 0x100000 + 4KB(0x1000) + 4KB(0x1000)
+	dwMappingAddress = 0;
+	for(i = 0; i < PAGE_MAXENTRYCOUNT * 64; i++){
+		kSetPageEntryData(&(pstPDPTEntry[i]), (i * (PAGE_DEFAULTSIZE >> 20)) >> 12, dwMappingAddress,
+				PAGE_FLAGS_DEFAULT | PAGE_FLAGS_PS, 0);
+		dwMappingAddress += PAGE_DEFAULTSIZE;
+	}
+
+}
+
+// Set base address, attribute flags
+void kSetPageEntryData( PTENTRY* pstEntry, DWORD dwUpperBaseAddress,
+		DWORD dwLowerBaseAddress, DWORD dwLowerFlags, DWORD dwUpperFlags){
+
+		pstEntry->dwAttributeAndLowerBaseAddress = dwLowerBaseAddress | dwLowerFlags;
+		pstEntry->dwUpperBaseAddressAndEXB = (dwUpperBaseAddress & 0xFF ) | dwUpperFlags; // & 0xFF(unsigned)
+
+}
