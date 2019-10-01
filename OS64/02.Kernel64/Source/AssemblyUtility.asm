f4a170d145ac75121efaccd515b965e761713525
dwordptr <bsms0418@icloud.com>
2019-10-01 07:59:13 +0000

    Update Kernel64/AssemblyUtility
    Add function to set GDTR, TR, IDTR register

--- OS64/02.Kernel64/Source/AssemblyUtility.asm
+++ OS64/02.Kernel64/Source/AssemblyUtility.asm
@@ -2,7 +2,7 @@
 
 SECTION .text
 
-global kInPortByte, kOutPortByte
+global kInPortByte, kOutPortByte, kLoadGDTR, kLoadTR, kLoadIDTR
 
 
 ; Read 1byte from port
@@ -28,6 +28,27 @@ kOutPortByte:
 	pop rax
 	pop rdx
 	ret
+; Set GDTR register
+; PARAM : GDT struct Address
+kLoadGDTR:
+	lgdt[rdi]	; Load param1(GDTR address) into processor
+	ret
+
+; Set TR register
+; PARAM : TSS Descriptor offset
+kLoadTR:
+	ltr di		; Load param1(TSS Descriptor offset) into processor
+	ret
+
+; Set IDTR register
+; PARAM : IDT struct address
+kLoadIDTR:
+	lidt[rdi]	;Load param1(IDTR address) into processor
+	ret
+
+
+
+
 
 
 
