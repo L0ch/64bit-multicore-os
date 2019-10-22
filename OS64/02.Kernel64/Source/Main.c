56e7d7697bf7b9cc53a5d0f27549e794ca0ffc67
dwordptr <bsms0418@icloud.com>
2019-10-22 02:11:33 +0000

    Update Kernel64 Main
    Load TSS and IDT for interrupt

--- OS64/02.Kernel64/Source/Main.c
+++ OS64/02.Kernel64/Source/Main.c
@@ -1,5 +1,6 @@
 #include "Types.h"
 #include "Keyboard.h"
+#include "Descriptor.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
 
@@ -12,26 +13,46 @@ void Main(void){
 	int i = 0;
 
 	kPrintString(0,10, "Switch To IA-32e Mode Success");
-	kPrintString(0,11, "IA-32e Kernel Start....................[Done]");
-	kPrintString(0,12, "Keyboard Activate......................[    ]");
+	kPrintString(0,11, "IA-32e Kernel Start..........................[Done]");
+
+	kPrintString(0,12, "GDT Initialize And Switch For IA-32e Mode....[    ]");
+	kInitializeGDTTableAndTSS();
+	kLoadGDTR(GDTR_STARTADDRESS);
+	kPrintString(46,12,"Done");
+
+	kPrintString(0,13, "TSS Segment Load.............................[    ]");
+	kLoadTR(GDT_TSSSEGMENT);
+	kPrintString(46,13,"Done");
+
+	kPrintString(0,14, "IDT Initialize...............................[    ]");
+	kInitializeIDTTables();
+	kLoadIDTR(IDTR_STARTADDRESS);
+	kPrintString(46,14,"Done");
+
+	kPrintString(0,15, "Keyboard Activate............................[    ]");
 
 	//kPrintString(0,13, "TEST");
 	if(kActivateKeyboard() == TRUE){
-		kPrintString(40,12,"Done");
+		kPrintString(46,15,"Done");
 		kChangeKeyboardLED(FALSE, FALSE, FALSE);
 	}
 	else{
-		kPrintString(45,12,"Fail");
+		kPrintString(46,15,"Fail");
 		while(1);
 	}
+
 	while(1){
 		if(kIsOutputBufferFull() == TRUE){
-			//kPrintString(0,13, "TEST");
 			bTemp = kGetKeyboardScanCode();
 
 			if(kConvertScanCodeToASCIICode(bTemp, &(vcTemp[0]), &bFlags) == TRUE){
 				if(bFlags & KEY_FLAGS_DOWN){
-					kPrintString(i++, 13, vcTemp);
+					kPrintString(i++, 16, vcTemp);
+					if(vcTemp[0] == '0'){
+						// Divide Error Exception
+						// Execute dummyHandler
+						bTemp = bTemp / 0;
+					}
 				}
 			}
 		}
